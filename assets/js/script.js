2c6520fbdd2bbaa590771e7a40b74922e119a23c
Menji <jennifer.menjivar18@gmail.com>
2021-06-20 11:40:19 -0400

    refactored data to use one call & 5 day forecast api's only

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
 var cityFormEl = document.querySelector("#city-form");
 var searchHistoryEl = document.querySelector("#search-history");
 var cities = [];
-// get the city
-// store into local storage
+var forecastArr = [];
 
-// function to get today's Weather Data
-var getWeatherData = function (city) {
+// function based on selected city to call API that has lat & lon coordinates neccesary to run main API
+var getLatAndLon = function (city) {
   var apiUrl =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
     city +
@@ -16,12 +15,15 @@ var getWeatherData = function (city) {
       //   check to see if response is ok
       if (response.ok) {
         return response.json().then(function (data) {
-          displayWeatherData(data);
-          getUv(data);
-
-          //   save city in search history
-          var saveCity = data.name;
-          saveCities(saveCity);
+          var name = data.name;
+          var lat = data.coord.lat;
+          var lon = data.coord.lon;
+          console.log(name);
+          getWeatherData(name, lat, lon);
+          //   //   getUv(data);
+          //   //   valid city was searched therefore we can save city in search history
+          //   var saveCity = data.name;
+          //   saveCities(saveCity);
         });
       } else {
         alert(
@@ -30,90 +32,91 @@ var getWeatherData = function (city) {
       }
     })
     .catch(function () {
-      alert("Unable to connect");
+      alert("ERROR: Unable to connect.");
     });
 };
 
-// function to get UV Index
-var getUv = function (weatherData) {
-  var lat = weatherData.coord.lat;
-  var lon = weatherData.coord.lon;
-
+// function to use lat & lon for API with ALL info for forcast and weather data
+var getWeatherData = function (cityName, cityLat, cityLon) {
   var apiUrl =
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-    lat +
+    cityLat +
     "&lon=" +
-    lon +
-    "&exclude=minutely,hourly,daily,alerts&appid=43cdeb3bdb5d7232fb98c9ed196e3be8";
+    cityLon +
+    "&exclude=minutely,hourly,alerts&units=imperial&appid=43cdeb3bdb5d7232fb98c9ed196e3be8";
+  var confrimCityName = cityName;
 
   fetch(apiUrl)
     .then(function (response) {
       if (response.ok) {
         return response.json().then(function (data) {
-          displayUvData(data);
+          console.log(confrimCityName);
+          displayWeatherData(confrimCityName, data);
+          createForecastArry(data);
         });
       } else {
-        alert("Could not fetch UV Index");
+        alert(
+          "There is an error retrieving Weather Data. Please tryagain later."
+        );
       }
     })
     .catch(function () {
-      alert("Unable to connect");
+      alert("ERROR: Unable to connect.");
     });
 };
 
-// function to display weather data
-var displayWeatherData = function (cityData) {
+// function to display weather data for current day
+var displayWeatherData = function (weatherCity, weatherData) {
+  console.log(weatherCity, weatherData);
   $("#weather-data-main").html("");
   $("#weather-data-info").html("");
 
-  // append City Name
-  var getCityName = cityData.name;
+  // get and append City Name
+  var getCityName = weatherCity;
   var cityName = $("<h2>").text(getCityName);
   cityName.addClass("weather-detail main city-name");
   $("#weather-data-main").append(cityName);
 
-  //   append Date
+  // get and append Date
   var getDate = moment().format("MMMM DD, YYYY");
   var date = $("<h4>").text(getDate);
   date.addClass("weather-detail main date");
   $("#weather-data-main").append(date);
 
-  //   append Weather Icon
-  var getWeatherIconId = cityData.weather[0].icon;
+  // get and append Weather Icon
+  var getWeatherIconId = weatherData.current.weather[0].icon;
   var iconUrl =
     "http://openweathermap.org/img/wn/" + getWeatherIconId + "@2x.png";
   var weatherIcon = $("<img>").attr("src", iconUrl);
   weatherIcon.addClass("weather-detail main img-icon right");
   $("#weather-data-main").append(weatherIcon);
 
-  // append Description
-  var getDescription = cityData.weather[0].description;
+  // get and append Description
+  var getDescription = weatherData.current.weather[0].description;
   var description = $("<span>").text("Description: " + getDescription);
   description.addClass("weather-detail");
   $("#weather-data-info").append(description);
 
-  // append Temp
-  var getCityTemp = cityData.main.temp;
+  // get and append Temp
+  var getCityTemp = weatherData.current.temp;
   var cityTemp = $("<span>").text("Temperature: " + getCityTemp + " \u00B0F");
   cityTemp.addClass("weather-detail");
   $("#weather-data-info").append(cityTemp);
 
-  // append Humidity
-  var getHumidity = cityData.main.humidity;
+  // get and append Humidity
+  var getHumidity = weatherData.current.humidity;
   var humidity = $("<span>").text("Humidity: " + getHumidity + "%");
   humidity.addClass("weather-detail");
   $("#weather-data-info").append(humidity);
 
-  // append Wind Speed
-  var getWindSpeed = cityData.wind.speed;
+  // get and append Wind Speed
+  var getWindSpeed = weatherData.wind_speed;
   var windSpeed = $("<span>").text("Wind Speed: " + getWindSpeed + " MPH");
   windSpeed.addClass("weather-detail");
   $("#weather-data-info").append(windSpeed);
-};
 
-// append UV Index
-var displayUvData = function (uvDataCall) {
-  var getUvIndex = uvDataCall.current.uvi;
+  //   get and append UVI
+  var getUvIndex = weatherData.current.uvi;
   var uvIndex = $("<span>").text("UV Index: " + getUvIndex);
 
   if (getUvIndex <= 2) {
@@ -123,36 +126,73 @@ var displayUvData = function (uvDataCall) {
   } else if (getUvIndex >= 8) {
     uvIndex.addClass("weather-detail extreme");
   }
-  //   append to page
+
   $("#weather-data-info").append(uvIndex);
 };
 
-// function to get 5 Day Forecast
-var getForecastData = function (city) {
-  var apiUrl =
-    "http://api.openweathermap.org/data/2.5/forecast?q=" +
-    city +
-    "&units=imperial&appid=43cdeb3bdb5d7232fb98c9ed196e3be8";
+// function to narrow down only the five days needed for forecast
+var createForecastArry = function (forecastData) {
+  var nextDayForecast = forecastData.daily[1];
+  forecastArr.push(nextDayForecast);
+  var secondDayForecast = forecastData.daily[2];
+  forecastArr.push(secondDayForecast);
+  var thirdDayForecast = forecastData.daily[3];
+  forecastArr.push(thirdDayForecast);
+  var fourthDayForecast = forecastData.daily[4];
+  forecastArr.push(fourthDayForecast);
+  var fifthDayForecast = forecastData.daily[5];
+  forecastArr.push(fifthDayForecast);
 
-  fetch(apiUrl)
-    .then(function (response) {
-      //   check to see if response is ok
-      if (response.ok) {
-        return response.json().then(function (data) {
-          displayForecastData(data);
-        });
-      } else {
-        return;
-      }
-    })
-    .catch(function () {
-      alert("Unable to connect");
-    });
+  displayForecastData(forecastArr);
 };
 
 // function to display Forcast data
-var displayForecastData = function (forcastData) {
-  console.log(forcastData);
+var displayForecastData = function (dailyForecastData) {
+  for (var i = 0; i < dailyForecastData.length; i++) {
+    console.log(dailyForecastData[i]);
+
+    // create date and append to forecast-results section
+    // var getDate = moment().add([++, "d").format("MMMM DD, YYYY");
+    // console.log(getDate);
+    // var date = $("<h4>").text(getDate);
+    // date.addClass("weather-detail main date");
+    // $("#weather-data-main").append(date);
+  }
+  //   $("#weather-data-main").html("");
+  //   $("#weather-data-info").html("");
+  //   // get and append Date
+  //   var getDate = moment().format("MMMM DD, YYYY");
+  //   var date = $("<h4>").text(getDate);
+  //   date.addClass("weather-detail main date");
+  //   $("#weather-data-main").append(date);
+  //   // get and append Weather Icon
+  //   var getWeatherIconId = weatherData.current.weather[0].icon;
+  //   var iconUrl =
+  //     "http://openweathermap.org/img/wn/" + getWeatherIconId + "@2x.png";
+  //   var weatherIcon = $("<img>").attr("src", iconUrl);
+  //   weatherIcon.addClass("weather-detail main img-icon right");
+  //   $("#weather-data-main").append(weatherIcon);
+  //   // get and append Description
+  //   var getDescription = weatherData.current.weather[0].description;
+  //   var description = $("<span>").text("Description: " + getDescription);
+  //   description.addClass("");
+  //   $("#weather-data-info").append(description);
+  //   // get and append Temp
+  //   var getCityTemp = weatherData.current.temp;
+  //   var cityTemp = $("<span>").text("Temperature: " + getCityTemp + " \u00B0F");
+  //   cityTemp.addClass("weather-detail");
+  //   $("#weather-data-info").append(cityTemp);
+  //   // get and append Humidity
+  //   var getHumidity = weatherData.current.humidity;
+  //   var humidity = $("<span>").text("Humidity: " + getHumidity + "%");
+  //   humidity.addClass("weather-detail");
+  //   $("#weather-data-info").append(humidity);
+  //   // get and append Wind Speed
+  //   var getWindSpeed = weatherData.wind_speed;
+  //   var windSpeed = $("<span>").text("Wind Speed: " + getWindSpeed + " MPH");
+  //   windSpeed.addClass("weather-detail");
+  //   $("#weather-data-info").append(windSpeed);
+  //   console.log(forecastData);
 };
 
 var createCityBtns = function (searchedCityName) {
@@ -204,8 +244,8 @@ var loadSavedCities = function () {
 // to get weather data based on search form
 $("#city-form").submit(function (event) {
   var cityInput = $("#city-input").val().trim();
-  getWeatherData(cityInput);
-  getForecastData(cityInput);
+  getLatAndLon(cityInput);
+  //   getForecastData(cityInput);
   event.preventDefault();
   document.querySelector("#city-form").reset();
 });
@@ -213,12 +253,8 @@ $("#city-form").submit(function (event) {
 // to get weather data based on search history buttons
 $("#search-history").on("click", ".city-btn", function (e) {
   var selectedCity = e.target.outerText;
-  getWeatherData(selectedCity);
-  getForecastData(selectedCity);
+  getLatAndLon(selectedCity);
+  //   getForecastData(selectedCity);
 });
-
-var testing = function (city) {
-  console.log(city);
-};
 
 loadSavedCities();
